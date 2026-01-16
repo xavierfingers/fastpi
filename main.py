4bf520bb1389a3223d44b9e323cbb0d30c40aedc
xavierfingers <xavierfingers@gmail.com>
2026-01-15 18:03:51 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from mpmath import mp
+def pi_dig(x, d):
+     summed = mp.mpf(0)
+     mp.dps = d
+     for i in range(0, 4000):
+         t1 = mp.mpf(4/(8*i+1))
+         t2 = mp.mpf(2/(8*i+4))
+         t3 = mp.mpf(1/(8*i+5))
+         t4 = mp.mpf(1/(8*i+6))
+         summed += mp.mpf(1/16**i*(t1-t2-t3-t4))
+         print(summed)
+print("Welcome to FastPI!")
+d = int(input("Enter digits: "))
+pi_dig(1, d)
