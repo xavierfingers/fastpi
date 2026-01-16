79695b9e79c32db7317c3dde18ff937522096376
xavierfingers <xavierfingers@gmail.com>
2026-01-15 18:11:04 -0700

    Added write to file

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from mpmath import mp
 def pi_dig(x, d):
      summed = mp.mpf(0)
      mp.dps = d
-     for i in range(0, 4000):
+     for i in range(d + 1):
          t1 = mp.mpf(4/(8*i+1))
          t2 = mp.mpf(2/(8*i+4))
          t3 = mp.mpf(1/(8*i+5))
          t4 = mp.mpf(1/(8*i+6))
          summed += mp.mpf(1/16**i*(t1-t2-t3-t4))
          print(summed)
+         with open("pi.txt", "w") as f:
+          f.write(summed)
+         
 print("Welcome to FastPI!")
 d = int(input("Enter digits: "))
 pi_dig(1, d)
